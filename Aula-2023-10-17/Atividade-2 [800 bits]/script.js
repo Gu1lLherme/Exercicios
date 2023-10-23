b31cc350f4c6ec70e1f5c47fb0da427b54d9d2a8
Guilherme Eugênio <101535832+Gu1lLherme@users.noreply.github.com>
2023-10-23 16:55:18 -0300

    finalizado

---
+++ Aula-2023-10-17/Atividade-2 [800 bits]/script.js
@@ -0,0 +1,38 @@
+
+var entrada = document.getElementById('instalacao-de-software');
+
+entrada.addEventListener("click", () => {
+    alert('Você Comprou o servço "Instalação de Software" por R$120,00')
+})
+
+var entrada = document.getElementById('reparo-eletronico');
+
+entrada.addEventListener("click", () => {
+    alert('Você Comprou o servço "Reparo em Eletrônicos" por R$400,00')
+})
+
+var entrada = document.getElementById('raspberry-pi4');
+
+entrada.addEventListener("click", () => {
+    alert('Você Comprou o produto "Raspberry Pi4" por R$180,00')
+})
+
+var entrada = document.getElementById('microcomponentes');
+
+entrada.addEventListener("click", () => {
+    alert('Você Comprou o servço "Reparo em Micro-componentes" por R$300,00')
+})
+
+var entrada = document.getElementById('upgrade-peca');
+
+entrada.addEventListener("click", () => {
+    alert('Você Comprou o servço "Upgrade de Peças Avulsas" por R$50,00')
+})
+
+var entrada_6 = document.getElementById('manutencao-redes');
+
+entrada_6.addEventListener("click", () => {
+    alert('Você Comprou o Servço "Manutenção em Redes de Computadores" por R$250,00')
+})
+
+
